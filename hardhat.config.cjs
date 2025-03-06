e7618af3c5799bdc60c59648ae7239b625748c9a
azeezabidoye <azeezabidoye@yahoo.com>
2025-03-06 12:08:56 +0100

    Hardhat Config Updated

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -2,5 +2,14 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  solidity: "0.8.23",
+  networks: {
+    crossfiTestnet: {
+      chainId: 4157,
+      url: "https://crossfi-testnet.g.alchemy.com/v2/wAz9j4RJUgEBiaMljD1yGbi45YRKBXTK",
+      accounts: [
+        "8dba19966d85ea2137505039c77d1e6ba35ab560797e51924f3edb939d9d2146",
+      ],
+    },
+  },
 };
