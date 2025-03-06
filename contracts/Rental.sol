3b1a76a10ce923874f1a9e9bd91e18c18d0e8dea
azeezabidoye <azeezabidoye@yahoo.com>
2025-03-06 12:30:10 +0100

    Contract updated

--- contracts/Rental.sol
+++ contracts/Rental.sol
@@ -23,4 +23,17 @@ contract Rental {
 
     // Mapping wallet addresses to each Renter
     mapping (address => Renter) public renters;
+
+    // Function for adding a Renter to the list of "Renters"
+    function addRenter(address payable walletAddress,
+        string memory firstName,
+        string memory lastName,
+        bool canRent,
+        bool active,
+        uint balance,
+        uint amountDue,
+        uint start,
+        uint end) public {
+            renters[walletAddress] = Renter(payable (walletAddress), firstName, lastName, canRent, active, balance, amountDue, start, end);
+    }
 }
