076fe1d091670882179f215d964f8dd0ab127f1d
azeezabidoye <azeezabidoye@yahoo.com>
2025-03-07 09:57:22 +0100

    Deploy Script updated

--- deploy/00-deploy-rental.cjs
+++ deploy/00-deploy-rental.cjs
@@ -1,1 +1,11 @@
-module.exports = async ({ getNamedAccounts, deployments }) => {};
+module.exports = async ({ getNamedAccounts, deployments }) => {
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+  await deploy("Rental", {
+    contract: "Rental",
+    from: deployer,
+    args: [],
+    log: true,
+  });
+};
+module.exports.tags = ["Rental"];
