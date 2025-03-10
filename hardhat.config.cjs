64251f149b4d9bb949035e107e9a019df1f43529
azeezabidoye <azeezabidoye@yahoo.com>
2025-03-10 14:17:58 +0100

    Config Updated

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -2,6 +2,8 @@ require("@nomicfoundation/hardhat-toolbox");
 require("hardhat-deploy");
 require("dotenv").config();
 
+const {} = process.env;
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
