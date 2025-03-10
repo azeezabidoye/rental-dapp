cc1ad4448648534db87cc8daae54631f751e860e
azeezabidoye <azeezabidoye@yahoo.com>
2025-03-10 14:15:58 +0100

    Config Updated

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -10,7 +10,7 @@ module.exports = {
   networks: {
     crossfiTestnet: {
       chainId: 4157,
-      url: "https://crossfi-testnet.g.alchemy.com/v2/wAz9j4RJUgEBiaMljD1yGbi45YRKBXTK",
+      url: "",
       accounts: [
         "8dba19966d85ea2137505039c77d1e6ba35ab560797e51924f3edb939d9d2146",
       ],
