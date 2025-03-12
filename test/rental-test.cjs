2578305130c276478199e052bb5d37592e772e36
azeezabidoye <azeezabidoye@yahoo.com>
2025-03-12 17:28:40 +0100

    Test script updated

--- test/rental-test.cjs
+++ test/rental-test.cjs
@@ -10,4 +10,22 @@ describe("Rental Smart Contract", function () {
     rental = await Rental.deploy();
     await rental.waitForDeployment();
   });
+
+  it("Should add a renter successfully", async function () {
+    await rental.addRenter(
+      renter.address,
+      "John",
+      "Doe",
+      true,
+      false,
+      ethers.parseEther("1"),
+      0,
+      0,
+      0
+    );
+
+    const addedRenter = await rental.renters(renter.address);
+    expect(addedRenter.firstName).to.equal("John");
+    expect(addedRenter.canRent).to.be.true;
+  });
 });
