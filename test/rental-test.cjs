f5864f5be6d0b6e58f8a0500606c1367a9894b94
azeezabidoye <azeezabidoye@yahoo.com>
2025-03-12 12:40:38 +0100

    Test Script Updated

--- test/rental-test.cjs
+++ test/rental-test.cjs
@@ -1,2 +1,13 @@
 const { ethers } = require("hardhat");
 const { expect } = require("chai");
+
+describe("Rental Smart Contract", function () {
+  let Rental, rental, owner, renter;
+
+  beforeEach(async function () {
+    Rental = await ethers.getContractFactory("Rental");
+    [owner, renter] = await ethers.getSigners();
+    rental = await Rental.deploy();
+    await rental.waitForDeployment();
+  });
+});
