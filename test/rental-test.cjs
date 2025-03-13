92ce3dc48ca4b297838fa9f58b50efb6a25b6cb7
azeezabidoye <azeezabidoye@yahoo.com>
2025-03-13 09:31:53 +0100

    Test script updated

--- test/rental-test.cjs
+++ test/rental-test.cjs
@@ -28,4 +28,43 @@ describe("Rental Smart Contract", function () {
     expect(addedRenter.firstName).to.equal("John");
     expect(addedRenter.canRent).to.be.true;
   });
+
+  it("Should allow renter to check out a car", async function () {
+    await rental.addRenter(
+      renter.address,
+      "John",
+      "Doe",
+      true,
+      false,
+      ethers.parseEther("1"),
+      0,
+      0,
+      0
+    );
+
+    await rental.checkOut(renter.address);
+    const updatedRenter = await rental.renters(renter.address);
+    expect(updatedRenter.active).to.be.true;
+    expect(updatedRenter.canRent).to.be.false;
+  });
+
+  it("Should allow renter to check in a car", async function () {
+    await rental.addRenter(
+      renter.address,
+      "John",
+      "Doe",
+      true,
+      false,
+      ethers.parseEther("1"),
+      0,
+      0,
+      0
+    );
+
+    await rental.checkOut(renter.address);
+    await rental.checkIn(renter.address);
+
+    const updatedRenter = await rental.renters(renter.address);
+    expect(updatedRenter.active).to.be.false;
+  });
 });
